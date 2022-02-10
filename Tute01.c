46c3f0cb46d97abf6018fb508cbbe488c9a2d887
IT21482502 <99388775+IT21482502@users.noreply.github.com>
2022-02-10 08:23:58 +0000

    fill the full code

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,21 @@
 #include <stdio.h>
 
 int main() {
-  
+  int num1,num2; // variables
+  int total;
+  float average;
+
+  printf("First subject marks : ");
+  scanf("%d", &num1); // getting first mark
+
+  printf("Second subject marks : ");
+  scanf("%d", &num2); //getting Second mark
+
+  total = num1 + num2;
+  average = total / 2.0; //Calculating averge
+
+  printf("Average of Two subject = %.2f ",average); //display the output
+
   return 0;
 }
 
